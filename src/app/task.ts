a79d25785669c74387946d298791196a98317ee9
Amit <amitaec94@gmail.com>
2021-07-25 15:21:20 +0530

    reverted

--- src/app/task.ts
+++
@@ -1,9 +0,0 @@
-export interface Task {
-    taskName;
-    parent_task_id;
-    priority;
-    start_date;
-    end_date;
-    project_id;
-    userId;
-}
--- src/app/user.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-    user_id;
-    first_name;
-    last_name;
-    employee_id;
-        
-}
